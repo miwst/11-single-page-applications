6b33014a893d28a343212fd7d91e0d9fe9b0ba3b
Scott <sdmccoy@gmail.com>
2017-05-22 13:44:59 -0700

    Created routes for index and about pages. Created controllers for index page to hide all and fade in filter and contents. Created controller for about page and built function to hide all and show about content

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,6 +3,12 @@ var app = app || {};
 
 (function(module) {
   const aboutController = {};
+  aboutController.loadAboutView = function(){
+    $('.tab-content').hide();
+    $(`#about`).fadeIn();
+
+  }
+
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
 
